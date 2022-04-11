6afcd3f9626bd28988f6744d2b7ad84baeef0da4
Michael Rossiter <michael.rossiter@shelbyfinance.com>
2022-04-11 23:27:20 +0100

    Lower to upper case convert for yes or no game using switch case

---
+++ 3.3/YORN.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define FALSE 0
+#define TRUE !FALSE
+
+int main()
+{
+    int done;
+    char ch;
+
+    done = FALSE;
+    printf("Would you like me to send your password to the bad guys?\n");
+    while(!done)
+    {
+        printf("Enter Y or N (Y/N)? ");
+        ch = tolower(getchar());
+        switch(ch)
+        {
+            case 'n':
+                printf("Well, then: your password is safe!\n");
+                done = TRUE;
+                break;
+            case 'y':
+                printf("Okay, sending your password!\n");
+                done = TRUE;
+                break;
+            default:
+                printf("You must enter either a Y or N!\n");
+                fflush(stdin);
+        }
+    }
+    return(0);
+}
