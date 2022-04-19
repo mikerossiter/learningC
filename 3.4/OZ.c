5522a4489ad14ff731c123ff68221e627da363b0
Michael Rossiter <goodbyezachry@gmail.com>
2022-04-19 22:43:05 +0100

    Added records to Oz DB

--- 3.4/OZ.c
+++ 3.4/OZ.c
@@ -4,11 +4,24 @@
 
 int main()
 {
-    char actor[] = "Judy Garland";
-    int age = 17;
-    char role[] = "Dorothy";
-    int line;
+    char actor1[] = "Judy Garland";
+    int age1 = 17;
+    char role1[] = "Dorothy";
+    
+    char actor2[] = "Ray Bolger";
+    int age2 = 35;
+    char role2[] = "Scarecrow";
+    
+    char actor3[] = "Bert Lahr";
+    int age3 = 44;
+    char role3[] = "Cowardly Lion";
+    
+    char actor4[] = "Jack Haley";
+    int age4 = 40;
+    char role4[] = "Tin Woodsman";
 
+    int line;
+    
     puts("Wizard of Oz Database!\n");
 
     /* draw the table heading */
@@ -17,7 +30,10 @@ int main()
     putchar('\n');
 
     /* display the data */
-    printf("%-15s %3d   %-15s\n",actor,age,role);
-
+    printf("%-15s %3d   %-15s\n",actor1,age1,role1);
+    printf("%-15s %3d   %-15s\n",actor2,age2,role2);
+    printf("%-15s %3d   %-15s\n",actor3,age3,role3);
+    printf("%-15s %3d   %-15s\n",actor4,age4,role4);
+    
     return(0);
 }
