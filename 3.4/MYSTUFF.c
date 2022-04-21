e240c806041fa5ce752de45581c210bd54f92073
Michael Rossiter <goodbyezachry@gmail.com>
2022-04-21 23:50:55 +0100

    Added update to structs in mystuff

--- 3.4/MYSTUFF.c
+++ 3.4/MYSTUFF.c
@@ -6,21 +6,30 @@ int main()
 
     struct stuff
     {
-        char letter;
-        int number;
-    };
+        char c;
+        int n;
+    } my, his, her;
 
-    struct stuff my;
+    /* Here is his info */
+    his.c = 'Y';
+    his.n = 199;
+    her.c = 'A';
+    her.n = 21;
 
     puts("Your Own Stuff");
 
     printf("Enter your favourite letter: ");
-    my.letter = getchar();
+    my.c = getchar();
 
     printf("Enter your favourite number: ");
-    scanf("%d",&my.number);
+    scanf("%d",&my.n);
 
-    printf("Your favourite letter is: %c\n",my.letter);
-    printf("And your favourite number is: %d\n",my.number);
+    printf("Your favourite letter is: %c\n",my.c);
+    printf("And your favourite number is: %d\n",my.n);
+    printf("His favourite letter is: %c\n",his.c);
+    printf("And his favourite number is: %d\n",his.n);
+    printf("Her favourite letter is: %c\n",her.c);
+    printf("And her favourite number is: %d\n",her.n);
+
     return(0);
 }
